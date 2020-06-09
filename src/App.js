73bee005a6281ffdf7ac533efe17ec9a2b2ee8b5
Rizatkam <rizalsatriakamal@gmail.com>
2020-06-09 22:28:57 +0700

    hoc tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import StockList from './components/StockList';
+import UserList from './components/UserList';
+import Hoc from './components/HOC';
+
+const StocksData = [
+  {
+      id: 1,
+      name: 'TCS'
+        
+  },
+  {
+      id: 2,
+      name: 'Infosys'
+  },
+  {
+      id: 3,
+      name: 'Reliance'
+  }
+];
+const UsersData = [
+  {
+      id: 1,
+      name: 'Krunal'
+        
+  },
+  {
+      id: 2,
+      name: 'Ankit'
+  },
+  {
+      id: 3,
+      name: 'Rushabh'
+  }
+];
+
+const Stocks = Hoc(
+  StockList,
+  StocksData
+);
+
+const Users = Hoc(
+  UserList,
+  UsersData
+);
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Users></Users>
+        <Stocks></Stocks>
+      </div>
+    )
+  }
 }
 
 export default App;
